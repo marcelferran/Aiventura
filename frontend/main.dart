7cdf3a62de2d7b51bc46214e6fb5d82bbd5b3068
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-05-14 13:54:13 -0600

    Update main.dart

--- frontend/main.dart
+++ frontend/main.dart
@@ -5,12 +5,4 @@ import 'app.dart';
 
 void main() {
   runApp(const AiventuraApp());
-=======
-// main.dart
-
-import 'package:flutter/material.dart';
-import 'app.dart';
-
-void main() {
-  runApp(const AiventuraApp());
 }
